a750dfd00e71a15683c705ae8026677787d2c192
Rafsn1987a <rafaelv1926@gmail.com>
2023-02-15 18:22:57 -0400

    Nav bars changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="fixed right-4 top-2">
       <Link href={'/'}>
-        <i className="fa-solid fa-house text-slate-400 text-lg opacity-20"></i>
+        <i className="fa-solid fa-house text-blue-400 text-lg opacity-20"></i>
       </Link>
     </header>
   );
--- src/pages/[testament]/[book]/[chapter].tsx
+++ src/pages/[testament]/[book]/[chapter].tsx
@@ -37,15 +37,10 @@ export default function Chapter() {
     }
   };
 
-  const prevChapter = () => {
-    const isFirstChapter = chapter === '1';
-    if (!isFirstChapter) router.push(`/${testament}/${book}/${Number(chapter) - 1}`);
-  };
-
-  const nextChapter = () => {
-    const isLastChapter = Number(chapter) === bookToParse.numOfChapters;
-    if (!isLastChapter) router.push(`/${testament}/${book}/${Number(chapter) + 1}`);
-  };
+  const isFirstChapter = chapter === '1';
+  const isLastChapter = Number(chapter) === bookToParse.numOfChapters;
+  const prevChapter = () => !isFirstChapter && router.push(`/${testament}/${book}/${Number(chapter) - 1}`);
+  const nextChapter = () => !isLastChapter && router.push(`/${testament}/${book}/${Number(chapter) + 1}`);
 
   if (chapter === undefined || bookToParse === undefined) return <>Loading...</>;
 
@@ -59,34 +54,42 @@ export default function Chapter() {
       </Head>
 
       {/* Nav bar */}
-      <div className="fixed left-0 bottom-0 px-4 w-full h-14 flex items-center justify-between bg-white border-2">
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-0 px-4 w-full h-14 flex items-center justify-between bg-white shadow-2xl border-t border-slate-50">
         <div className="flex items-center gap-x-4">
           {/* Previous chapter */}
-          <button onClick={prevChapter} className="py-1 px-2 border text-slate-400 rounded-lg">
+          <button
+            onClick={prevChapter}
+            disabled={isFirstChapter}
+            className="py-1 px-2 border text-slate-400 bg-white rounded-lg disabled:bg-slate-50"
+          >
             &lt;
           </button>
 
           {/* Home */}
           <Link href={`/${testament}`}>
-            <i className="fa-solid fa-book-bible text-lg text-slate-600"></i>
+            <i className="fa-solid fa-book-bible text-2xl text-blue-400 "></i>
           </Link>
         </div>
 
         <Link
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-4 py-1 bg-white shadow-md rounded-lg border-t"
           href={`/${testament}/${book}`}
         >{`${bookToParse.book} : ${chapter}`}</Link>
 
         <div className="flex items-center gap-x-4">
           {/* Tool box */}
           <div>
-            <button onClick={changeFontSize} className="px-2 py-1 border rounded-lg">
+            <button onClick={changeFontSize} className="px-2 py-1 border bg-white rounded-lg">
               Aa
             </button>
           </div>
 
           {/* Next chapter */}
-          <button onClick={nextChapter} className="px-2 py-1 border text-slate-400 rounded-lg">
+          <button
+            onClick={nextChapter}
+            disabled={isLastChapter}
+            className="px-2 py-1 border text-slate-400 bg-white rounded-lg disabled:bg-slate-50"
+          >
             &gt;
           </button>
         </div>
